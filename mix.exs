e1943842f87019a8cd52808d7ac5a53a992bcf24
Julian Doherty <julian@juliandoherty.com>
2025-04-07 23:13:04 +1000

    add MIT license to mix

--- mix.exs
+++ mix.exs
@@ -8,7 +8,8 @@ defmodule AttrAccessor.MixProject do
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      docs: docs()
+      docs: docs(),
+      licenses: "MIT"
     ]
   end
 
